f591e76380fcc742b9f2d9d0d201ecd823d3d574
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-09-14 15:44:09 -0300

    Entendendo o Objeto Request

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('email');
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+
+
     }
 }
