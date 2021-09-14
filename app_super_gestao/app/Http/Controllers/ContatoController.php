1d672044b5aa9aa27c13185e13cbdbc445d3c97e
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-09-14 17:04:16 -0300

    Construindo método para salvar formulário no banco

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -9,6 +9,7 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
+        /*
         echo '<pre>';
         print_r($request->all());
         echo '</pre>';
@@ -16,7 +17,18 @@ class ContatoController extends Controller
         echo '<br>';
         echo $request->input('email');
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+        */
 
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //print_r($contato->getAttributes());
+        $contato->save();
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
 
     }
 }
