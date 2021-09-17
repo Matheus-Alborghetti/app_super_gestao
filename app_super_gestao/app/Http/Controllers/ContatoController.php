4913241922046bfcd6b9736207f765a1aa4832a6
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-09-17 15:33:49 -0300

    Validação de campos com (required)

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -9,9 +9,19 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        $contato = new SiteContato();
-        $contato->create($request->all());
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request) {
+
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+        //SiteContato::create($request->all());
 
     }
 }
