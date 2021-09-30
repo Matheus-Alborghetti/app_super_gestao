59cd03db7bf81ac37e5c871e7314e731ca6d09f6
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-09-30 16:16:03 -0300

    Personalizando mensagens nos formulários

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
@@ -14,7 +15,35 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar');
     }
 
-    public function adicionar() {
-        return view('app.fornecedor.adicionar');
+    public function adicionar(Request $request) {
+
+        $msg = '';
+
+        if($request->input('_token') != '' ){
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nomemin' => 'O campo deve ter no mínimo 3 caracteres',
+                'nomemax' => 'O campo deve ter no máximo 40 caracteres',
+                'ufmin' => 'O campo deve ter no mínimo 2 caracteres',
+                'ufmax' => 'O campo deve ter no máximo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Cadastro realizado com sucesso';
+        }
+
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
