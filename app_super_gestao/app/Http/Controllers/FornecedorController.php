fdea7f41b2d06864e10895be796d9130b36bd698
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-10-04 08:57:32 -0300

    Paginação de Registros

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -17,16 +17,16 @@ class FornecedorController extends Controller
             ->where('site', 'like', '%'.$request->input('site').'%')
             ->where('uf', 'like', '%'.$request->input('uf').'%')
             ->where('email', 'like', '%'.$request->input('email').'%')
-        ->get();
+            ->paginate(2);
 
-        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores, 'request' => $request->all()]);
     }
 
     public function adicionar(Request $request) {
 
         $msg = '';
 
-        if($request->input('_token') != '' ){
+        if($request->input('_token') != '' && $request->input('id') == ''){
             $regras = [
                 'nome' => 'required|min:3|max:40',
                 'site' => 'required',
@@ -51,6 +51,26 @@ class FornecedorController extends Controller
             $msg = 'Cadastro realizado com sucesso';
         }
 
+        if($request->input('_token') != '' && $request->input('id') != ''){
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizada com sucesso';
+            } else {
+                $msg = 'Erro ao tentar atualizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.adicionar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
+
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+    public function editar($id, $msg = '') {
+
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/listar.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/listar.blade.php
@@ -41,11 +41,14 @@
                                 <td>{{ $fornecedor->uf }}</td>
                                 <td>{{ $fornecedor->email }}</td>
                                 <td>Excluir</td>
-                                <td>Editar</td>
+                                <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
+
+                {{ $fornecedores->appends($request)->links() }}
+
             </div>
 
         </div>
