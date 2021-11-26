23df881288b671a78c9020644e0376e61bf02efb
Matheus-Alborghetti <matheus.alborghetti@gmail.com>
2021-11-26 16:00:01 -0300

    Lazy Loading e Eager Loading

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -17,10 +17,9 @@ class ProdutoController extends Controller
      */
     public function index(Request $request)
     {
-        $produtos = Item::paginate(10);
+        $produtos = Item::with(['itemDetalhe'])->paginate(10);
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]);
-
     }
 
     /**
--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -20,6 +20,7 @@
 
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
+
                 <table border="1" width="100%">
                     <thead>
                         <tr>
